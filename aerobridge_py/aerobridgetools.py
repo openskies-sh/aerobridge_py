78195b6b683ab10f0b050b076047be81847fa435
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2022-02-19 12:55:46 +0000

    Update aerobridgetools.py

--- aerobridge_py/aerobridgetools.py
+++ aerobridge_py/aerobridgetools.py
@@ -2,6 +2,23 @@ import requests, json
 
 # Version: 0.0.1
 
+class AuthorityCredentialsGetter():
+	''' All calls to the Aerobridge requires credentials from a oauth server, in this case this is Flight Passport  '''
+	def __init__(self, client_id:str, client_secret:str, audience:str, token_url:str):
+
+		self.client_id = client_id
+		self.client_secret = client_secret
+		self.audience = audience
+		self.token_url = token_url
+
+	def get_credentials(self):  
+		payload = {"grant_type":"client_credentials","client_id": self.client_id,"client_secret": self.client_secret,"audience": self.audience,"scope": 'aerobridge.read'}          
+		url = self.token_url
+		
+		token_data = requests.post(url, data = payload)
+		t_data = token_data.json()     
+		return t_data
+
 class AerobridgeClient():
 	'''
 	This a a Python client that make calls to the Aerobridge API
@@ -35,10 +52,6 @@ class AerobridgeClient():
 
 	def download_flight_plan(self, plan_id):
 		''' This method downloads the flight plan in the form of a plan file given the flight plan id '''
-		raise NotImplementedError
-
-	def get_latest_firmware(self, firmware_id):
-		''' This method downloads the latest firmware  in the form of a plan file given the flight plan id '''
 		raise NotImplementedError
 
 	def get_all_aircrafts(self):
